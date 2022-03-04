de96a08a1ac564a3b91a6583e36d1183271bade3
jinicha <jinichaa@gmail.com>
2022-03-03 17:36:16 -0800

    Load csv

--- schema.sql
+++ schema.sql
@@ -1,38 +1,48 @@
-DROP DATABASE IF EXISTS reviews;
-CREATE DATABASE reviews;
+DROP DATABASE IF EXISTS reviewsdb;
+CREATE DATABASE reviewsdb;
 
-DROP TABLE IF EXISTS reviews, reviews_photos, characteristics, characteristic_reviews;
+\c reviewsdb;
+
+DROP TABLE IF EXISTS reviews, photos, characteristics, characteristic_reviews;
 
 CREATE TABLE reviews (
  id BIGSERIAL PRIMARY KEY,
  product_id INTEGER,
  rating INTEGER NOT NULL,
- date VARCHAR(30) NOT NULL,
- summary VARCHAR(60) NOT NULL,
+ date BIGINT NOT NULL,
+ summary VARCHAR(250) NOT NULL,
  body VARCHAR(1000) NOT NULL,
- recommend INTEGER NOT NULL,
- reported INTEGER NOT NULL,
+ recommend BOOLEAN NOT NULL,
+ reported BOOLEAN NOT NULL,
  reviewer_name VARCHAR(60) NOT NULL,
- reviewer_email VARCHAR(60) NOT NULL,
+ reviewer_email VARCHAR(75) NOT NULL,
  response VARCHAR(1000) NOT NULL,
  helpfulness INTEGER NOT NULL
 );
 
-CREATE TABLE reviews_photos (
+CREATE TABLE photos (
  id BIGSERIAL PRIMARY KEY,
  review_id INTEGER NOT NULL,
- url VARCHAR(200)
+ url VARCHAR(200),
+ FOREIGN KEY (review_id) REFERENCES reviews(id)
 );
 
 CREATE TABLE characteristics (
  id BIGSERIAL PRIMARY KEY,
  product_id INTEGER NOT NULL,
- name VARCHAR(60) NOT NULL
+ name VARCHAR(75) NOT NULL
 );
 
 CREATE TABLE characteristic_reviews (
   id BIGSERIAL PRIMARY KEY,
   characteristic_id INTEGER NOT NULL,
   review_id INTEGER NOT NULL,
-  value VARCHAR(20) NOT NULL
+  value VARCHAR(50) NOT NULL,
+  FOREIGN KEY (characteristic_id) REFERENCES characteristics(id),
+  FOREIGN KEY (review_id) REFERENCES reviews(id)
 );
+
+COPY reviews FROM '/Users/jinicha/Desktop/seip2201/sdc/csv/reviews.csv' DELIMITER ',' CSV HEADER;
+COPY photos FROM '/Users/jinicha/Desktop/seip2201/sdc/csv/reviews_photos.csv' DELIMITER ',' CSV HEADER;
+COPY characteristics FROM '/Users/jinicha/Desktop/seip2201/sdc/csv/characteristics.csv' DELIMITER ',' CSV HEADER;
+COPY characteristic_reviews FROM '/Users/jinicha/Desktop/seip2201/sdc/csv/characteristic_reviews.csv' DELIMITER ',' CSV HEADER;
