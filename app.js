3bb001e110156b877a46a26e3453d7861c30ea78
jinicha <jinichaa@gmail.com>
2022-03-09 23:23:31 -0800

    Move app.js to root dir, add route '/'

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const mountRoutes = require('../routes');
+const mountRoutes = require('./routes');
 
 const app = express();
 
 const PORT = 3001;
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('This is server');
+});
 
 mountRoutes(app);
 
